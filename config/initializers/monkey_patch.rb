3772c87b72e1e9e759a847d88e6c3653689b2b5e
Michael Kelly <michaelkelly322@gmail.com>
2020-02-28 17:43:20 -0500

    Remove monkey_path

--- config/initializers/monkey_patch.rb
+++
@@ -1,9 +0,0 @@
-module Shipit
-  class Hook < ActiveRecord::Base
-    CONTENT_TYPES = {
-      'json' => 'application/json',
-      'form' => 'application/x-www-form-urlencoded',
-    }
-  end
-end
-
