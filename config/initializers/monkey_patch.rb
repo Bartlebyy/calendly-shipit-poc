3418b9703b2d999f81710499477f4b8c3f274da8
Michael Kelly <michaelkelly322@gmail.com>
2020-02-28 16:44:21 -0500

    Unfreezes content type hash

--- config/initializers/monkey_patch.rb
+++ config/initializers/monkey_patch.rb
@@ -1,6 +1,9 @@
 module Shipit
-  def user_access_tokens_key
-    ENV['SECRET_KEY_BASE'].byteslice(0, 32)
-    # (secrets.user_access_tokens_key.presence || secrets.secret_key_base).byteslice(0, 32)
+  class Hook < ActiveRecord::Base
+    CONTENT_TYPES = {
+      'json' => 'application/json',
+      'form' => 'application/x-www-form-urlencoded',
+    }
   end
 end
+
