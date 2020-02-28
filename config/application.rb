fa2b64f075e5366d3a26facd890f45d2465af7f8
Michael Kelly <michaelkelly322@gmail.com>
2020-02-28 16:11:32 -0500

    Disable persistent connections in Pubsubstub

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Shipit
     config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
     config.active_record.cache_versioning = false
 
+    Pubsubstub.use_persistent_connections = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
